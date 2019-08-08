f61853fc1b11db0238c650b662431d484a527052
Your Name <you@example.com>
2019-08-08 17:17:19 -0400

    created Dashboard component

---
+++ my-app/src/Components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Dashboard = () => {
+
+    return (
+        <div>
+            <button>Strike</button>
+            <button>Ball</button>
+            <button>Hit</button>
+            <button>Foul</button>
+        </div>
+    )
+}
+
+export default Dashboard
